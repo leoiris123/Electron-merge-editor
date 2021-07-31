8658bacf6d7dcf398eb4b1be09f477a4c24942de
jijun.lei <jijun.lei@mybogames.com>
2021-07-31 11:28:03 +0800

    初始化-3

---
+++ src/components/BackBtn.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+         <el-button @click="goBack" class="back-btn"  icon="el-icon-arrow-left"></el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BackBtn',
+
+    components: { },
+
+    directives: {},
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+ 
+    methods:{
+        goBack(){
+            this.$router.back()
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+    .back-btn{
+        position: absolute;
+        right: 10px;
+        top: 10px;
+    }
+</style>
