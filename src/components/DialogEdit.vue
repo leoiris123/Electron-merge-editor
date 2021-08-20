9e6d75b6c59ead745e75998147e026afd9f1240a
jijun.lei <jijun.lei@mybogames.com>
2021-08-20 13:47:59 +0800

    预览配置1

--- src/components/DialogEdit.vue
+++ src/components/DialogEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div ref="box"> </div>
+    <div ref="box" class="box"></div>
     <!-- <button @click="test">test</button> -->
     <el-collapse v-model="currentName" accordion @change="handleChange">
       <el-collapse-item
@@ -20,148 +20,149 @@
                   :key="index1"
                   :label="character"
                 >
-                <el-row type="flex">
-                <el-col :span=10>  
-                    <el-row>
-                    {{ character }}
-                  </el-row>
-                  <el-row>
-                    <el-col
-                      >选择角色：<el-select
-                        v-model="dialogEditList[item.id][character].char"
-                        @change="handleSelectChange"
-                        filterable
-                        clearable
-                        placeholder="请选择"
-                      >
-                        <el-option
-                          v-for="item in Object.keys(configuration)"
-                          :key="item"
-                          :label="item"
-                          :value="item"
-                        >
-                        </el-option>
-                      </el-select>
-                    </el-col>
-                  </el-row>
-                  <!-- 选择皮肤： -->
-                  <el-row
-                    v-if="
-                      dialogEditList[item.id][character].char == ''
-                        ? false
-                        : true
-                    "
-                  >
-                    <el-col
-                      >选择皮肤：
-                      <el-select
-                        v-if="
-                          dialogEditList[item.id][character].char == ''
-                            ? false
-                            : true
-                        "
-                        v-model="dialogEditList[item.id][character].emotion"
-                        @change="handleSelectChange"
-                        filterable
-                        clearable
-                        placeholder="请选择"
-                      >
-                        <!-- configuration[dialogEditList[item.id][character].char].skin.default -->
-                        <!-- :disabled="dialogEditList[item.id][character].char==''?true:false" -->
-                        <div>
-                          <el-option
-                            v-for="item in dialogEditList[item.id]
-                              ? configuration[
-                                  dialogEditList[item.id][character].char
-                                ].skin.default
-                              : null"
-                            :key="item"
-                            :label="item"
-                            :value="item"
+                  <el-row type="flex">
+                    <el-col :span="10">
+                      <el-row>
+                        {{ character }}
+                      </el-row>
+                      <el-row>
+                        <el-col
+                          >选择角色：<el-select
+                            v-model="dialogEditList[item.id][character].char"
+                            @change="handleSelectChange"
+                            filterable
+                            clearable
+                            placeholder="请选择"
                           >
-                          </el-option>
-                        </div>
-                      </el-select>
-                    </el-col>
-                  </el-row>
-                  <!-- 选择音效 -->
-                  <el-row
-                    v-if="
-                      dialogEditList[item.id][character].char == ''
-                        ? false
-                        : true
-                    "
-                  >
-                    <el-col
-                      >选择音效：
-                      <el-select
+                            <el-option
+                              v-for="item in Object.keys(configuration)"
+                              :key="item"
+                              :label="item"
+                              :value="item"
+                            >
+                            </el-option>
+                          </el-select>
+                        </el-col>
+                      </el-row>
+                      <!-- 选择皮肤： -->
+                      <el-row
                         v-if="
                           dialogEditList[item.id][character].char == ''
                             ? false
                             : true
                         "
-                        v-model="dialogEditList[item.id][character].audio"
-                        @change="handleSelectChange"
-                        filterable
-                        clearable
-                        placeholder="请选择"
                       >
-                        <!-- configuration[dialogEditList[item.id][character].char].skin.default -->
-                        <!-- :disabled="dialogEditList[item.id][character].char==''?true:false" -->
-                        <div>
-                          <el-option
-                            v-for="item in dialogEditList[item.id]
-                              ? configuration[
-                                  dialogEditList[item.id][character].char
-                                ].audios
-                              : null"
-                            :key="item"
-                            :label="item"
-                            :value="item"
+                        <el-col
+                          >选择皮肤：
+                          <el-select
+                            v-if="
+                              dialogEditList[item.id][character].char == ''
+                                ? false
+                                : true
+                            "
+                            v-model="dialogEditList[item.id][character].emotion"
+                            @change="handleSelectChange"
+                            filterable
+                            clearable
+                            placeholder="请选择"
                           >
-                          </el-option>
-                        </div>
-                      </el-select>
-                    </el-col>
-                  </el-row>
-                  <!-- 选择为主 -->
-                  <el-row>
-                    <el-col>
-                      <el-checkbox
-                        @change="handleSelectChange"
+                            <!-- configuration[dialogEditList[item.id][character].char].skin.default -->
+                            <!-- :disabled="dialogEditList[item.id][character].char==''?true:false" -->
+                            <div>
+                              <el-option
+                                v-for="item in dialogEditList[item.id]
+                                  ? configuration[
+                                      dialogEditList[item.id][character].char
+                                    ].skin.default
+                                  : null"
+                                :key="item"
+                                :label="item"
+                                :value="item"
+                              >
+                              </el-option>
+                            </div>
+                          </el-select>
+                        </el-col>
+                      </el-row>
+                      <!-- 选择音效 -->
+                      <el-row
                         v-if="
                           dialogEditList[item.id][character].char == ''
                             ? false
                             : true
                         "
-                        v-model="dialogEditList[item.id][character].isMain"
-                        >选择为主</el-checkbox
                       >
+                        <el-col
+                          >选择音效：
+                          <el-select
+                            v-if="
+                              dialogEditList[item.id][character].char == ''
+                                ? false
+                                : true
+                            "
+                            v-model="dialogEditList[item.id][character].audio"
+                            @change="handleSelectChange"
+                            filterable
+                            clearable
+                            placeholder="请选择"
+                          >
+                            <!-- configuration[dialogEditList[item.id][character].char].skin.default -->
+                            <!-- :disabled="dialogEditList[item.id][character].char==''?true:false" -->
+                            <div>
+                              <el-option
+                                v-for="item in dialogEditList[item.id]
+                                  ? configuration[
+                                      dialogEditList[item.id][character].char
+                                    ].audios
+                                  : null"
+                                :key="item"
+                                :label="item"
+                                :value="item"
+                              >
+                              </el-option>
+                            </div>
+                          </el-select>
+                        </el-col>
+                      </el-row>
+                      <!-- 选择为主 -->
+                      <el-row>
+                        <el-col>
+                          <el-checkbox
+                            @change="handleSelectChange"
+                            v-if="
+                              dialogEditList[item.id][character].char == ''
+                                ? false
+                                : true
+                            "
+                            v-model="dialogEditList[item.id][character].isMain"
+                            >选择为主</el-checkbox
+                          >
+                        </el-col>
+                      </el-row>
+                      <!-- 显示名字-->
+                      <el-row>
+                        <el-col>
+                          <el-checkbox
+                            @change="handleSelectChange"
+                            v-if="
+                              dialogEditList[item.id][character].char == ''
+                                ? false
+                                : true
+                            "
+                            v-model="
+                              dialogEditList[item.id][character].showName
+                            "
+                            >显示名字</el-checkbox
+                          >
+                        </el-col>
+                      </el-row>
+                    </el-col>
+                    <el-col class="preview" :span="14">
+                      <div class="preview">
+                        这是预览动画
+                      </div>
                     </el-col>
                   </el-row>
-                  <!-- 显示名字-->
-                  <el-row>
-                    <el-col>
-                      <el-checkbox
-                        @change="handleSelectChange"
-                        v-if="
-                          dialogEditList[item.id][character].char == ''
-                            ? false
-                            : true
-                        "
-                        v-model="dialogEditList[item.id][character].showName"
-                        >显示名字</el-checkbox
-                      >
-                    </el-col>
-                  </el-row> 
-                </el-col >
-                <el-col class="preview" :span=14> 
-                  <div class="preview">
-                     这是预览动画
-                  </div>
-                 
-                </el-col>
-                </el-row>
                 </el-tab-pane>
               </el-tabs>
             </el-tab-pane>
@@ -188,6 +189,7 @@
 </template>
 
 <script>
+import * as PIXI from "pixi.js";
 import event from "../../script/tool/event";
 import initDialogEditVO from "../../script/vo/initDialogEditVO";
 export default {
@@ -202,28 +204,7 @@ export default {
       currentName: ["1"],
       selectDialogGroupName: "",
       characterList: ["left1", "right1", "left2", "right2"],
-      options: [
-        {
-          value: "选项1",
-          label: "黄金糕",
-        },
-        {
-          value: "选项2",
-          label: "双皮奶",
-        },
-        {
-          value: "选项3",
-          label: "蚵仔煎",
-        },
-        {
-          value: "选项4",
-          label: "龙须面",
-        },
-        {
-          value: "选项5",
-          label: "北京烤鸭",
-        },
-      ],
+
       value: "",
       configuration: this.$store.getters["configuration/configurationGet"],
     };
@@ -298,7 +279,39 @@ export default {
     event.$on("saveAllDialog", () => {
       this.$store.dispatch("section/SET_DIALOG_EDIT_LIST", this.dialogEditList);
     });
-    // let app = new PIXI.Application({width: 256, height: 256});
+    const app = new PIXI.Application({
+      width: 600,
+      height: 600,
+      // antialias: true,
+      // transparent: false,
+      // resolution: 2,
+      // autoResize: true,
+    });
+    this.$refs.box.appendChild(app.view);
+    // const loader = new PIXI.Loader();
+    const loader = PIXI.Loader.shared;// Loader.shared内置的单例loader
+    loader.add("demo1", "./image-1.png").load(setup);
+    loader.onProgress.add((loader) => {
+});
+    function setup() {
+      let test = new PIXI.Container();
+      let demo1 = new PIXI.Sprite(loader.resources["demo1"].texture);
+      demo1.width = 300;
+      demo1.height = 300;
+      demo1.x = 60;
+      demo1.y = 60;
+      console.log(demo1,'-----demo1')
+      console.log(test,'-----test')
+      test.addChild(demo1)
+
+      // demo1.position.set(50, 50);
+      app.stage.addChild(test);
+
+      console.log(app.stage,'-----app')
+
+    }
+
+    console.log("demo1");
   },
 
   methods: {
@@ -322,8 +335,8 @@ export default {
 };
 </script>
 
-<style lang="scss" >
-.preview{
+<style lang="scss">
+.preview {
   background: rgb(122, 122, 206);
   width: 500px;
   height: 500px;
