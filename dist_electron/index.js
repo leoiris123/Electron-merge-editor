b885900170bcf71380f03dc0d5a1869c1904dc17
jijun.lei <jijun.lei@mybogames.com>
2021-09-13 15:37:47 +0800

    优化

--- dist_electron/index.js
+++ dist_electron/index.js
@@ -1777,7 +1777,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var elec
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__(/*! /Users/admin_aric/Downloads/EASY/merge-editor2/src/background.js */\"./src/background.js\");\n\n\n//# sourceURL=webpack:///multi_./src/background.js?");
+eval("module.exports = __webpack_require__(/*! /Users/admin_aric/Downloads/EASY/merge-editor/src/background.js */\"./src/background.js\");\n\n\n//# sourceURL=webpack:///multi_./src/background.js?");
 
 /***/ }),
 
