af5ee859a14d8502421ad608a5d7d6ebc7e584bb
jijun.lei <jijun.lei@mybogames.com>
2021-10-19 11:56:40 +0800

    部分修改-待优化

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -244,14 +244,6 @@ body > .el-container {
   margin-bottom: 40px;
 }
 
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  /* line-height: 260px; */
-}
-
-.el-container:nth-child(7) .el-aside {
-  /* line-height: 320px; */
-}
 .top-list {
   background-color: #8195b9;
   padding: 0;
