4acadfc579180d7b594c5597e3ca81aea5fdaba2
jijun.lei <jijun.lei@mybogames.com>
2022-01-20 10:46:54 +0800

    土耳其语bug修复

--- script/load/load.js
+++ script/load/load.js
@@ -26,7 +26,7 @@ const XMLList = {
   dialogConfig: {
     localPath: read("/dialogConfig.xlsx"),
     storeScript: "section/SET_DIALOG_LIST",
-    sortType: { header: ["property", "id", "en", "zh", "pt", "ko", "de", "kr", "fr", "it", "ja", "es", "ru", "zh_t"] },
+    sortType: { header: ["property", "id", "en", "zh", "pt", "ko", "de", "tr", "fr", "it", "ja", "es", "ru", "zh_t"] },
   },
   textConfig: {
     localPath: "/textConfig.xlsx",
